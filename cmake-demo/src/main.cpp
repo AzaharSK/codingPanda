a5d987491910c91e3f48deb603d69e6c8bebb3a1
Azahar Sk <azahar.sk.uddin@gmail.com>
2022-05-06 18:05:02 +0530

    cmake demo for Factorial cpp program

---
+++ cmake-demo/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include "modules/Factorial.hpp"
+#include "modules/Factorial.hpp"
+
+using namespace std;
+
+
+// Driver code
+int main()
+{
+    int num = 5;
+    cout << "Factorial of "
+         << num << " is " << factorial(num) << endl;
+    return 0;
+}
